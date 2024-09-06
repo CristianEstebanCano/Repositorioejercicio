3c0f6ec729af2ea1f16936298cd783acba1d741c
CristianEstebanCano <canox09@gmail.com>
2024-09-06 17:06:57 -0500

    senocoseno

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner teclado =new Scanner(System.in);
-        double numero1,numero2,suma,resta,multi,raiz;
+        double numero1,numero2,suma,resta,multi,raiz1,raiz2,seno,coseno;
 
         System.out.println("Escribe el primer valor: ");
         numero1 = teclado.nextDouble();
@@ -16,12 +16,20 @@ public class Main {
         suma=numero1+numero2;
         resta=numero1-numero2;
         multi=numero1-numero2;
-        raiz= Math.sqrt(suma);
+
+        raiz1= Math.sqrt(numero1);
+        raiz2= Math.sqrt(numero2);
+
+        seno=Math.sin(numero1);
+        coseno=Math.cos(numero2);
 
         System.out.println("La suma es " + suma);
         System.out.println("La resta es " + resta);
         System.out.println("La multiplicacion es " +multi);
-        System.out.println("La raiz es " + raiz);
+        System.out.println("La raiz del numero 1 es " + raiz1);
+        System.out.println("La raiz del numero 2 es " + raiz2);
+        System.out.println("El seno del numero 1 es " + seno);
+        System.out.println("El coseno del numero 2 es " + coseno);
 
 
     }
