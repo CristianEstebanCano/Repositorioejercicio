70ff378d04a5a262b1d7b2bfb275d727548f6f21
CristianEstebanCano <canox09@gmail.com>
2024-09-06 16:48:41 -0500

    Commit Ejercicio

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner teclado =new Scanner(System.in);
+        double numero1,numero2,suma,resta,multi;
+
+        System.out.println("Escribe el primer valor: ");
+        numero1 = teclado.nextDouble();
+        System.out.println("Escribe el segundo valor: ");
+        numero2 = teclado.nextDouble();
+
+        suma=numero1+numero2;
+        resta=numero1-numero2;
+        multi=numero1-numero2;
+
+        System.out.println("La suma es " + suma);
+        System.out.println("La resta es " + resta);
+        System.out.println("La multiplicacion es " +multi);
+
+
+    }
+}
