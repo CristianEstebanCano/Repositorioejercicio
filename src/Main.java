030d081a1f4b9bb3c931090ef82e81fcdb4c85c6
CristianEstebanCano <canox09@gmail.com>
2024-09-06 16:54:27 -0500

    raiz

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner teclado =new Scanner(System.in);
-        double numero1,numero2,suma,resta,multi;
+        double numero1,numero2,suma,resta,multi,raiz;
 
         System.out.println("Escribe el primer valor: ");
         numero1 = teclado.nextDouble();
@@ -16,10 +16,12 @@ public class Main {
         suma=numero1+numero2;
         resta=numero1-numero2;
         multi=numero1-numero2;
+        raiz= Math.sqrt(suma);
 
         System.out.println("La suma es " + suma);
         System.out.println("La resta es " + resta);
         System.out.println("La multiplicacion es " +multi);
+        System.out.println("La raiz es " + raiz);
 
 
     }
